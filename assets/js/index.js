fad7f6d520000440618aa66503488ed4054b6669
Bibiwei Pere <bibiweipere@gmail.com>
2023-06-27 01:15:47 +0100

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,17 @@
 // ?Hamburger menu
 const hamburger = document.querySelector(".hamburger");
-const navMenus = document.querySelector(".nav-menu-1");
 const navMenu = document.querySelector(".nav-menu");
 const navClose = document.querySelector(".nav-close");
 const body = document.querySelector(".body");
+const navbar = document.querySelector(".navbar");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-  navMenus.classList.toggle("active");
   navMenu.classList.toggle("active");
   navClose.classList.toggle("active");
   body.classList.toggle("active");
+  navbar.classList.toggle("active");
 }
 
 // Close navbar when link is clicked
@@ -21,9 +21,9 @@ closeNav.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
   hamburger.classList.remove("active");
-  navMenus.classList.remove("active");
   navMenu.classList.remove("active");
   body.classList.remove("active");
+  navbar.classList.remove("active");
 }
 
 // Close navbar when link is clicked
@@ -34,25 +34,26 @@ navLink.forEach((n) => n.addEventListener("click", closeMenu));
 function closeMenu() {
   hamburger.classList.remove("active");
   navClose.classList.remove("active");
-  navMenus.classList.remove("active");
   body.classList.remove("active");
+  navbar.classList.remove("active");
   navMenu.classList.remove("active");
 }
 
-// Dropdown
-const dropdown = document.querySelector(".dropdown");
-const openMenu = document.querySelector(".drop-menu");
-
-dropdown.addEventListener("click", dropMenu);
-function dropMenu() {
-  openMenu.classList.toggle("active");
-}
-
 // Dropdown 2
-const dropdowns = document.querySelector(".dropdown-2");
-const openMenus = document.querySelector(".drop-menu-2");
+const desplegable = document.querySelectorAll(".desplegable");
+const menu_desplegable = document.querySelectorAll(".menu_desplegable");
+const arrowDown = document.querySelectorAll(".arrow-down");
 
-dropdowns.addEventListener("click", dropMenus);
-function dropMenus() {
-  openMenus.classList.toggle("active");
+for (let i = 0; i < desplegable.length; i++) {
+    const desplegables = desplegable[i];
+    
+    desplegables.addEventListener("click", ()=>{
+        desplegables.classList.toggle("active")
+        arrowDown[i].classList.toggle("active");
+        if(desplegables.classList.contains("active")){
+            menu_desplegable[i].style.display = "flex";   
+        } else {
+            menu_desplegable[i].style.display = "none";  
+        }
+    })
 }
